750d5a65bac189bee51dacf6726521b5ce9d7341
17punchisama <144563556+17punchisama@users.noreply.github.com>
2023-09-10 04:22:43 +0700

    add text to test

--- lib/screen/start_screen.dart
+++ lib/screen/start_screen.dart
@@ -21,7 +21,7 @@ class StartScreen extends StatelessWidget {
 
                 const Text(
                   //showing app name
-                  'APP NAME',
+                  'APP NAME อะไรก็ไม่รู้',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 36,
