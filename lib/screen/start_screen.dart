59ddd148dba43c6f13eba1c28625b360c46dc61a
17punchisama <144563556+17punchisama@users.noreply.github.com>
2023-09-10 15:06:19 +0700

    change app name

--- lib/screen/start_screen.dart
+++ lib/screen/start_screen.dart
@@ -21,7 +21,7 @@ class StartScreen extends StatelessWidget {
 
                 const Text(
                   //showing app name
-                  'APP NAME hi Im punyee',
+                  'แสดงชื่อแอพ',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 36,
