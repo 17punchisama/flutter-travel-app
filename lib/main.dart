8c6fd4487ce8e589c2243b95df978d72100469dc
17punchisama <144563556+17punchisama@users.noreply.github.com>
2023-09-10 21:08:25 +0700

    edit startscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app_project/screen/reset_password_screen.dart';
 import 'package:travel_app_project/screen/start_screen.dart';
 
 void main() {
-  runApp(const StartScreen());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
-  Widget build(BuildContext context) => const MaterialApp(
-        home: StartScreen(),
-      );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Define your app's initial route here
+      initialRoute: '/',
+      routes: {
+        '/': (context) => StartScreen(),
+        '/reset_password': (context) => ResetPasswordScreen(),
+        // Add more routes as needed for your app
+      },
+    );
+  }
 }
