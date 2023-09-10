0a4531ea8db0da7a1dd70f8d7c3a0aec235375f7
17punchisama <144563556+17punchisama@users.noreply.github.com>
2023-09-10 14:29:44 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,7 +15,7 @@ void main() {
     // Build our app and trigger a frame.
 
     // Verify that our counter starts at 0.
-    expect(find.text('10'), findsOneWidget);
+    expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
