0dbea960147358da7fccaa0018bd8cd375366b2a
17punchisama <144563556+17punchisama@users.noreply.github.com>
2023-09-10 23:37:39 +0700

    add sign in complete

---
+++ lib/screen/sign_in_complete.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignInComplete extends StatelessWidget {
+  const SignInComplete({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
