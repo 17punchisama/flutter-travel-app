307de7917bf51f5f5bf91414ad372db7f48c596b
17punchisama <144563556+17punchisama@users.noreply.github.com>
2023-09-10 21:47:51 +0700

    navigator home and sign in

---
+++ lib/screen/sign_in_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              'Sign in',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
